440d76943c90b8bebf1d93c5c0b4cbaa90c5b6ed
Tom Dugan <thomas@aadwebb.com>
2019-09-24 11:45:30 -0500

    set up routes

---
+++ src/config/store.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+
+})
+
+const store = createStore(
+  rootReducer,
+  // CHECK TO SEE IF FUNCTION EXISTS (if browser has dev tools extension) AND, IF SO, CALL IT
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+export default store
