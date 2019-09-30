6471822766bcfac0927b21ce3289f6b6a85fba24
Tom Dugan <thomas@aadwebb.com>
2019-09-30 16:12:54 -0500

    quantity not incrementing

--- src/features/cart/index.js
+++ src/features/cart/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-export const cartItemsWithQuantity = (cartItems) => {
+export const cartItemsWithQuantities = (cartItems) => {
   return cartItems.reduce((acc, item) => {
     const filteredItem = acc.filter(item2 => item2.id === item.id)[0]
     filteredItem !== undefined
--- src/features/product-listing/index.js
+++ src/features/product-listing/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProductListItem from './product-list-item';
 import { connect } from 'react-redux';
-import { cartItemsWithQuantity } from '../cart';
+import { cartItemsWithQuantities } from '../cart';
 
 function ProductListing(props) {
   return <div className='product-listing'>
@@ -10,7 +10,7 @@ function ProductListing(props) {
         <ProductListItem
           product={product}
           addToCart={props.addToCart}
-          cart={cartItemsWithQuantity(props.cart)}
+          cart={cartItemsWithQuantities(props.cart)}
         />)
     }
   </div>
